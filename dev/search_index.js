182fdd1b437850a76fd42f558703d9e9935c2ff5
Documenter.jl <documenter@juliadocs.github.io>
2022-02-22 16:39:53 +0000

    build based on ab74fce

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MaxwellPDEs","category":"page"},{"location":"#MaxwellPDEs","page":"Home","title":"MaxwellPDEs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MaxwellPDEs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MaxwellPDEs]","category":"page"}]
+}
